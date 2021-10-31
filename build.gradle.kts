32413a4d01cc29b98cf87a10581f7b9a0f21080d
Rahul Behera <thebehera@fb.com>
2021-10-30 23:41:04 -0700

    add simpler subscribe

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.ditchoom:mqtt-base-models:1.0.7")
+                implementation("com.ditchoom:mqtt-base-models:1.0.8")
                 implementation("com.ditchoom:buffer:1.0.46")
             }
         }
--- src/commonMain/kotlin/com/ditchoom/mqtt5/controlpacket/ControlPacketV5Factory.kt
+++ src/commonMain/kotlin/com/ditchoom/mqtt5/controlpacket/ControlPacketV5Factory.kt
@@ -4,6 +4,7 @@ package com.ditchoom.mqtt5.controlpacket
 
 import com.ditchoom.buffer.PlatformBuffer
 import com.ditchoom.buffer.ReadBuffer
+import com.ditchoom.mqtt.topic.Filter
 import com.ditchoom.mqtt.controlpacket.*
 import com.ditchoom.mqtt.controlpacket.format.ReasonCode
 
@@ -46,6 +47,25 @@ object ControlPacketV5Factory : ControlPacketFactory {
         )
         val variableHeader = PublishMessage.VariableHeader(topicName, packetIdentifier, properties)
         return PublishMessage(fixedHeader, variableHeader, payload)
+    }
+
+    override fun subscribe(
+        packetIdentifier: Int,
+        topicFilter: CharSequence,
+        maximumQos: QualityOfService,
+        noLocal: Boolean,
+        retainAsPublished: Boolean,
+        retainHandling: ISubscription.RetainHandling,
+        serverReference: CharSequence?,
+        userProperty: List<Pair<CharSequence, CharSequence>>
+    ): ISubscribeRequest {
+        val subscription = Subscription(Filter(topicFilter), maximumQos)
+        return subscribe(
+            packetIdentifier,
+            setOf(subscription),
+            serverReference,
+            userProperty
+        )
     }
 
     override fun subscribe(
